825f5e2db7067dca7306966e60da7b9ac6f41163
USHA <ushawin2020@gmail.com>
2025-12-27 13:08:07 +0530

    Set dark theme as default

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 const themeToggle = document.getElementById('themeToggle');
 const html = document.documentElement;
 
-// Check for saved theme preference or default to 'light' mode
-const currentTheme = localStorage.getItem('theme') || 'light';
+// Check for saved theme preference or default to 'dark' mode
+const currentTheme = localStorage.getItem('theme') || 'dark';
 html.setAttribute('data-theme', currentTheme);
 
 // Update icon based on theme
